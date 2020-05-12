afbbd2102b574a603a279ccb6c405ff5db1dcd61
Patryk Skowron <pat.skowron@gmail.com>
2020-05-12 12:16:27 +0200

    Add some lessons

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ To run php on apache server just type:
 ```bash
 make run
 ```
+
+# Links
+
+online php sandbox: https://wtools.io/php-sandbox
+## tutorials
+
+https://www.php.net/manual/en/getting-started.php
+https://www.w3schools.com/php/php_syntax.asp
--- forms/postForm.php
+++ forms/postForm.php
@@ -1,10 +1,6 @@
-
-
 <?php
 // use here the id of the form you want to capture.
-        $microBlog = $_POST['microBlog'];
-        echo "Look What you've just posted!";
-        echo $microBlog;
-        echo "Are you proud? :)";
-
+        $color = $_POST['color'];
+        echo "chosed color ${color}";
+        $car = get_car_based_on_color($color);
 ?>
